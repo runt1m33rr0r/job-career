4d521d12664cfd8430532103cda8336527016572
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-10-22 00:26:44 +0300

    made a very basic markdown editor

---
+++ jobseeker-ui/src/Input.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import CodeMirror from "codemirror";
+
+import "codemirror/mode/markdown/markdown";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "./Input.css";
+
+function Input() {
+  const textArea = React.createRef();
+
+  useEffect(() => {
+    CodeMirror.fromTextArea(textArea.current, {
+      lineNumbers: true,
+      mode: "markdown",
+      theme: "dracula"
+    });
+  }, [textArea]);
+
+  return <textarea ref={textArea}></textarea>;
+}
+
+export default Input;
