8a2cd612445386a47f220cc13425dc96c83332dd
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-11-06 17:59:14 +0200

    added applications filter option

--- jobseeker-ui/src/components/Applications.jsx
+++ jobseeker-ui/src/components/Applications.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import { makeStyles } from "@material-ui/core/styles";
 import ApplicationModal from "./ApplicationModal";
 import ItemsList from "./ItemsList";
 import ApplicationListItem from "./ApplicationListItem";
@@ -31,8 +36,8 @@ const applications = [
     lastName: "last name",
     lastModified: "some date",
     notice: {
-      id: 1,
-      title: "some title",
+      id: 2,
+      title: "some title2",
       category: "some category",
       company: "some company",
       content: "some content",
@@ -42,12 +47,45 @@ const applications = [
   }
 ];
 
-const Applications = () => (
-  <ItemsList
-    items={applications}
-    popupElement={ApplicationModal}
-    listItemElement={ApplicationListItem}
-  />
-);
+const useStyles = makeStyles(theme => ({
+  container: {
+    width: "100%",
+    height: "80vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 300
+  }
+}));
+
+function Applications() {
+  const classes = useStyles();
+  const [noticeFilter, setNoticeFilter] = useState(1);
+
+  const handleFilterChange = event => setNoticeFilter(event.target.value);
+
+  return (
+    <div className={classes.container}>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Notice filter</InputLabel>
+        <Select value={noticeFilter} onChange={handleFilterChange}>
+          {applications.map(application => (
+            <MenuItem key={application.id} value={application.notice.id}>
+              {application.notice.title}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <ItemsList
+        items={applications}
+        popupElement={ApplicationModal}
+        listItemElement={ApplicationListItem}
+      />
+    </div>
+  );
+}
 
 export default Applications;
