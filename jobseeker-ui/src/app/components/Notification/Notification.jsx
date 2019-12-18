354dcfcbcab05439253d313e8176688a0fccbf9a
runt1m33rr0r <19938633+runt1m33rr0r@users.noreply.github.com>
2019-12-18 12:03:01 +0200

    more appropriate notification color

--- jobseeker-ui/src/app/components/Notification/Notification.jsx
+++ jobseeker-ui/src/app/components/Notification/Notification.jsx
@@ -5,12 +5,11 @@ import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import ErrorIcon from "@material-ui/icons/Error";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
-import { green } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   success: {
-    backgroundColor: green[600]
+    backgroundColor: theme.palette.primary.dark
   },
   error: {
     backgroundColor: theme.palette.error.dark
