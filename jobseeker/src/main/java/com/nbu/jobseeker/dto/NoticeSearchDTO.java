ae0894e9dcb861409e75fe74ea2085c6f8c0c4a9
Nikolay Boshnakov <n_boshnakov@abv.bg>
2019-12-23 21:58:10 +0200

    Now possible to search for notices with multiple categories. Fixed login not returning no user message

--- jobseeker/src/main/java/com/nbu/jobseeker/dto/NoticeSearchDTO.java
+++ jobseeker/src/main/java/com/nbu/jobseeker/dto/NoticeSearchDTO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class NoticeSearchDTO {
     private List<String> keywords;
-    private String category;
+    private List<String> categories;
     private List<String> statuses;
 
     public List<String> getStatuses() {
@@ -15,12 +15,12 @@ public class NoticeSearchDTO {
         this.statuses = statuses;
     }
 
-    public String getCategory() {
-        return category;
+    public List<String> getCategories() {
+        return categories;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
     }
 
     public List<String> getKeywords() {
